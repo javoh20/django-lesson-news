77dedf50ddce46aba5a04657486c6b5f4e85d2eb
javoh20 <javoh7806@gmail.com>
2024-02-14 20:08:58 +0500

    admin panel

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'published_time', 'status']
+    list_filter = ['status', 'created_time', 'published_time']
+    prepopulated_fields = {'slug':('title',)}
+    search_fields = ['title', 'disc']
+
+
+# admin.site.register(News)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    ordering = ['id']
